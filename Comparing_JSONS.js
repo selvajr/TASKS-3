f3e265098b54200d8f905e0bce562c6aaae8763b
Selva-C <postbox221@outlook.com>
2024-05-11 21:27:57 +0530

    Corrected Obj Comparison

--- Comparing_JSONS.js
+++ Comparing_JSONS.js
@@ -1,5 +1,21 @@
 let obj1 = { name: "Person 1", age: 5 };
 let obj2 = { age: 5, name: "Person 1" };
 
-const equal = JSON.stringify(obj1) === JSON.stringify(obj2);
-console.log(equal);
+function compareObjects(obj1, obj2) {
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    for (let key of keys1) {
+        if (obj1[key] !== obj2[key]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(compareObjects(obj1, obj2)); 
